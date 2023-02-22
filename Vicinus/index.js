00b297b9ba4c59ac8dac502d7a6cee898496f97b
Virtopeanu Sebastian <f_seby@yahoo.com>
2023-02-22 20:32:38 +0200

    Added labels

--- Vicinus/index.js
+++ Vicinus/index.js
@@ -1,4 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const labels = [
+    "Fresh",
+    "Locally-sourced",
+    "Organic",
+    "Sustainable",
+    "Farm-to-people",
+    "Seasonal",
+    "Natural",
+    "Grass-fed",
+    "Delicious",
+    "Farm-fresh",
+    "Local",
+    "Pesticide-free",
+    "Antibiotic-free",
+    "Hormone-free",
+    "Freshly-harvested",
+    "Juicy",
+    "Tender",
+    "Freshly-baked",
+  ];
+
+  const labelElements = labels.map((label) => {
+    const labelElement = document.createElement("strong");
+    labelElement.textContent = label;
+    labelElement.style.paddingRight = "2px";
+    return labelElement;
+  });
+
+  // Get the container element by its ID
+  const container = document.getElementById("label-container");
+
+  // Add the label elements to the container element
+  labelElements.forEach((labelElement) => {
+    container.appendChild(labelElement);
+  });
   let accordionHeaders = document.querySelectorAll(".accordion-header");
   console.log(accordionHeaders);
   accordionHeaders.forEach((header) => {
