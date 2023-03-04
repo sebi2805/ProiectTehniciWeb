d8053feb7975102e0efdb465ad66479dd9cb57f6
Virtopeanu Sebastian <f_seby@yahoo.com>
2023-03-04 10:54:29 +0200

    Added changes for step 2

--- Vicinus/index.js
+++ Vicinus/index.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const satisfactionElements = document.querySelectorAll("td#satisfaction");
+  console.log(satisfactionElements);
+  satisfactionElements.forEach((element) => {
+    const satisfactionPercentage = parseInt(element.textContent.slice(0, -1)); //pentru a nu lua si % din text
+    if (satisfactionPercentage < 50) {
+      element.style.color = "red";
+    } else if (satisfactionPercentage < 75) {
+      element.style.color = "orange";
+    } else {
+      element.style.color = "green";
+    }
+  });
+  //////////////////////////////////////////////////////////////
+
   const labels = [
     "Fresh",
     "Locally-sourced",
@@ -26,22 +40,19 @@ document.addEventListener("DOMContentLoaded", () => {
     labelElement.style.paddingRight = "2px";
     return labelElement;
   });
+  //////////////////////////////////////////////////////////////////////
 
-  // Get the container element by its ID
   const container = document.getElementById("label-container");
 
-  // Add the label elements to the container element
   labelElements.forEach((labelElement) => {
     container.appendChild(labelElement);
   });
   let accordionHeaders = document.querySelectorAll(".accordion-header");
-  console.log(accordionHeaders);
+
   accordionHeaders.forEach((header) => {
     header.addEventListener("click", (event) => {
-      // Get the corresponding content element
       let content = event.target.nextElementSibling;
 
-      // Toggle the content's visibility
       if (content.style.display === "block") {
         content.style.display = "none";
       } else {
